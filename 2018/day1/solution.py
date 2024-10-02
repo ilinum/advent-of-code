b7b1026ee1c1f9ba4653cb102f3113ba45d6fbb9
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2024-10-02 07:07:09 -0500

    2018 day 1

--- 2018/day1/solution.py
+++ 2018/day1/solution.py
@@ -1,26 +1,30 @@
-import sys
-from typing import List
+def solve_p1(lines: list[str]) -> object:
+    values = [int(line) for line in lines]
+    return sum(values)
 
-
-def part1(lines: List[str]) -> int:
-    freq = 0
-    for line in lines:
-        freq += int(line)
-    return freq
-
-
-def part2(lines: List[str]) -> int:
-    freq = 0
+def solve_p2(lines: list[str]) -> object:
+    values = [int(line) for line in lines]
     seen = set()
-    idx = 0
-    while freq not in seen:
-        seen.add(freq)
-        freq += int(lines[idx])
-        idx += 1
-        if idx == len(lines):
-            idx = 0
-    return freq
+    cur = 0
+    i = 0
+    while cur not in seen:
+        seen.add(cur)
+        cur += values[i]
+        i = (i+1) % len(values)
+    return cur
 
 
+
+def process_file(filename: str) -> None:
+    print(f"processing '{filename}'")
+    with open(filename, "r") as f:
+        lines = list(map(lambda x: x.lower().strip(), f.readlines()))
+    print(solve_p1(lines))
+    print(solve_p2(lines))
+
+
+# Part 1: 00:01:52
+# Part 2: 00:06:06
 if __name__ == '__main__':
-    print(part2(sys.stdin.readlines()))
+    process_file("sample.in")
+    process_file("input.in")
