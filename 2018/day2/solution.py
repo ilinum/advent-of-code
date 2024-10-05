0cc56042d3db8528ef0a5c5cb221ae5d59c15656
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2024-10-05 12:40:13 -0500

    2018 day 2

--- 2018/day2/solution.py
+++ 2018/day2/solution.py
@@ -1,34 +1,47 @@
-import sys
-from typing import List
-import collections
-
-
-def part2(lines: List[str]) -> str:
-    if not lines:
-        raise RuntimeError('lines is empty')
-    line_len = len(lines[0].strip())
-    for index_to_remove in range(line_len):
-        seen = set()
-        for line in lines:
-            line = line.strip()
-            stripped = line[:index_to_remove] + line[index_to_remove + 1:]
-            if stripped in seen:
-                return stripped
-            seen.add(stripped)
-    raise RuntimeError()
-
-
-def part1(lines: List[str]) -> int:
-    twos = 0
-    threes = 0
+def solve_p1(lines: list[str]) -> object:
+    id_freqs = []
     for line in lines:
-        counter = collections.Counter(line)
-        if 2 in counter.values():
-            twos += 1
-        if 3 in counter.values():
-            threes += 1
-    return twos * threes
+        freqs = {}
+        for c in line:
+            freqs[c] = freqs.get(c, 0) + 1
+        id_freqs.append(freqs)
+    num_doubles = 0
+    num_triples = 0
+    for freqs in id_freqs:
+        if 2 in freqs.values():
+            num_doubles += 1
+        if 3 in freqs.values():
+            num_triples += 1
+    return num_doubles * num_triples
+
+def common_letters(a: str, b: str) -> str:
+    result = []
+    for x, y in zip(a, b):
+        if x == y:
+            result.append(x)
+    return "".join(result)
+
+def solve_p2(lines: list[str]) -> object:
+    for i in range(len(lines)):
+        a = lines[i]
+        for b in lines[i+1:]:
+            assert len(a) == len(b)
+            common = common_letters(a, b)
+            if len(common) == len(a)-1:
+                return common
+    assert False, "no matches found"
 
 
+def process_file(filename: str) -> None:
+    print(f"processing '{filename}'")
+    with open(filename, "r") as f:
+        lines = list(map(lambda x: x.lower().strip(), f.readlines()))
+    print(solve_p1(lines))
+    print(solve_p2(lines))
+
+
+# Part 1: 00:04:25
+# Part 2: 00:11:29
 if __name__ == '__main__':
-    print(part2(sys.stdin.readlines()))
+    process_file("sample.in")
+    process_file("input.in")
