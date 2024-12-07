f473b9b784d9d90d4e914b487540d9336b3a7442
Stas Ilinskiy <ilinskiy.sv@gmail.com>
2024-12-07 08:54:33 -0800

    update template to report processing time

--- template/solution.py
+++ template/solution.py
@@ -1,3 +1,6 @@
+import time
+
+
 def solve_p1(lines: list[str]) -> object:
     return None
 
@@ -10,12 +13,14 @@ def process_file(filename: str) -> None:
     print(f"processing '{filename}'")
     with open(filename, "r") as f:
         lines = list(map(lambda x: x.strip().lower(), f.readlines()))
+    start = time.time()
     print(solve_p1(lines))
     print(solve_p2(lines))
+    print(f"processed file '{filename}' in {time.time() - start:.2f}s")
 
 
 # Part 1:
 # Part 2:
-if __name__ == '__main__':
+if __name__ == "__main__":
     process_file("sample.in")
     process_file("input.in")
